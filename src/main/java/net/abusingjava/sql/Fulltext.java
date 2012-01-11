72b583da5c84b32c418a7014a9d65d239f969286
Julian Fleischer <julian.fleischer@fu-berlin.de>
2012-01-11 02:12:41 +0100

    Organized imports.

--- src/main/java/net/abusingjava/sql/Fulltext.java
+++ src/main/java/net/abusingjava/sql/Fulltext.java
@@ -4,10 +4,6 @@ import net.abusingjava.Author;
 import net.abusingjava.Since;
 import net.abusingjava.Version;
 
-import net.abusingjava.Author;
-import net.abusingjava.Since;
-import net.abusingjava.Version;
-
 @Author("Julian Fleischer")
 @Version("2012-01-04")
 @Since("2012-01-04")
